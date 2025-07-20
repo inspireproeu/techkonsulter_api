ec28e9a1050b8dfd5572f69382d394925f476a4e
anandsdn <anandsdn@gmail.com>
2025-07-21 00:52:38 +0530

    added screen field in export computer

--- extensions/Functions/mapvalues/index.js
+++ extensions/Functions/mapvalues/index.js
@@ -10,24 +10,33 @@ const assignValues = {
         return ["NOTEBOOK PC", "15.6 INCH", "14 INCH", "MOBILE WORKSTATION PC", "WITH RADEON GRAPHICS", "HP Compaq ", "HP ", "Dell ", "Lenovo ", "Compaq ", "HP Compaq", "HP", "Dell", "Lenovo", "Compaq"];
     },
     removeComplainData: () => {
-        return ["SCREEN MIN SCR", "REAR MIN SCR", "EDGE MIN SCR", "chassi: glue", "chassi:glue", "chassi glue", "lid: glue", "Chassi min scr", "pad.worn", "rem BIOS pwd", "lid: min scr", "Reset to factory defaults", "Chassi.Glue", "Powerwashed", "lid; min scr", "chassi; min scr", "kbd; no TP", "pad; worn", "Factory reset", "HW", "POS unit", "POS unit,", "kbd; worn ", "kbd; worn", "Missing adapter", "pad:worn", "lid; min scr", "chassi: min scr", "lid, min scr", "pad, worn", "KBD: worn", "chassii: min scr", "chassi: min scr", "NO TP", "Missing TP", "pad: worn", "chassi. min scr", "MDM UNLOCKED", "REM MDM LOCK", "rem bios pwd,", "rem bios pwd;", "BIOS PW REM MIN", "BIOS PW REM", "REM COMPUTRACE", ",BIOS PW REM MIN", ",BIOS PW REM", ",REM COMPUTRACE", 'min', ' min', 'MIN', ' MIN'];
+        return ["SCREEN MIN SCR", "REAR MIN SCR", "EDGE MIN SCR", "chassi: glue", "chassi:glue", "chassi glue", "lid: glue", "Chassi min scr", "pad.worn", "rem BIOS pwd", "lid: min scr", "Reset to factory defaults", "Chassi.Glue", "Powerwashed", "lid; min scr", "chassi; min scr", "kbd; no TP", "pad; worn", "Factory reset", "HW", "POS unit", "POS unit,", "kbd; worn ", "kbd; worn", "Missing adapter", "pad:worn", "lid; min scr", "chassi: min scr", "lid, min scr", "pad, worn", "KBD: worn", "chassii: min scr", "chassi: min scr", "NO TP", "Missing TP", "pad: worn", "chassi. min scr", "MDM UNLOCKED", "REM MDM LOCK", "rem bios pwd,", "rem bios pwd;", "BIOS PW REM MIN", "BIOS PW REM", "REM COMPUTRACE", ",BIOS PW REM MIN", ",BIOS PW REM", ",REM COMPUTRACE", 'min', ' min', 'MIN', ' MIN',
+            ",COMPUTRACE REM MIN",
+            "COMPUTRACE REM MIN",
+            ",COMPUTRACE REM",
+            "COMPUTRACE REM",
+            "TOUCHSCREEN MIN",
+            ",TOUCHSCREEN MIN"];
     },
-    ASSIGNSTOCKLISTVALUES: async (data, type,api_name) => {
+    removeMultipleWords: async (str, searchList) => {
+        if (str) {
+            const words = str.toUpperCase().split(" ");
+            const filtered = words.filter(word => !searchList.includes(word));
+            return filtered.join(" ");
+        }
+    },
+    ASSIGNSTOCKLISTVALUES: async (data, type, api_name) => {
         let rows = []
         data.forEach(async (item) => {
             item.target_price = item.target_price ? Math.round(item.target_price) : '';
-            assignValues.removeModelData().forEach((removable) => {
-                if (item.model) {
-                    item.model = item.model.toUpperCase().replace(removable, "").trim();
-                    return item.model;
-                }
-            })
-            assignValues.removeprocessorData().forEach((removable) => {
-                if (item.processor) {
-                    item.processor = item.processor.toUpperCase().replace(removable, "").trim();
-                    return item.processor;
-                }
-            })
+            const removeMultipleWords = (str, searchList) => {
+                const words = str.split(" ");
+                const filtered = words.filter(word => !searchList.includes(word));
+                return filtered.join(" ");
+            };
+            if (item.model) {
+                item.model = removeMultipleWords(item.model, assignValues.removeModelData());
+            }
             if (item.asset_type === 'COMPUTER PARTS') {
                 item.asset_type = 'PARTS COMPUTER'
             }
@@ -59,19 +68,18 @@ const assignValues = {
             if (item.complaint_from_app) {
                 item.complaint_from_app = item.complaint_from_app.replace(/;/g, " ");
             }
-
-            assignValues.removeComplainData().forEach((removable) => {
-                if (item.complaint) {
-                    item.complaint = item.complaint.toUpperCase().replace(removable.toUpperCase(), "").trim();
-                    return item.complaint;
-                }
-            })
-            assignValues.removeComplainData_A_Grade().forEach((removable) => {
-                if (item.grade === "A" && item.complaint) {
-                    item.complaint = item.complaint.toUpperCase().replace(removable.toUpperCase(), "").trim();
-                    return item.complaint;
-                }
-            })
+            if (item.complaint && item.grade !== "A") {
+                item.complaint = removeMultipleWords(item.complaint, assignValues.removeComplainData());
+            }
+            if (item.grade === "A" && item.complaint) {
+                item.complaint = removeMultipleWords(item.complaint, assignValues.removeComplainData());
+            }
+            if (item.complaint?.toUpperCase().includes('LOCK BIOS PWD MED')) {
+                item.complaint = item.complaint.toUpperCase().replace('LOCK BIOS PWD MED', "LOCK BIOS PWD");
+            }
+            if (item.complaint?.toUpperCase().includes('DEF KBD MIN')) {
+                item.complaint = item.complaint.toUpperCase().replace('DEF KBD MIN', "DEF KBD");
+            }
             if (item.complaint) {
                 //----------- Delete first character of string if it is comma from string if its firts
                 while (item.complaint.charAt(0) === ',') {
@@ -82,8 +90,8 @@ const assignValues = {
                 if (complaint.toUpperCase() === 'MIN') {
                     item.complaint = ''; // set cmplaint as empty when value shows only MIN
                 }
-            }
 
+            }
 
             if (item.asset_type && (item.asset_type.toUpperCase() === 'COMPUTER')) {
                 let hddtext = ["no hdd", "hdd rem", "hdd crash", "hdd fail"]
@@ -134,19 +142,19 @@ const assignValues = {
                         || dataDestruction.includes("not erased (")
                         || dataDestruction.toLowerCase() === "not erased") {
                         delete item
-                    }else {
-                    stocks_report.push(item)
+                    } else {
+                        stocks_report.push(item)
                     }
                 } else {
-                    if(api_name){
+                    if (api_name) {
                         delete item.data_destruction
                     }
                     stocks_report.push(item)
                 }
             } else {
-                                  if(api_name){
-                        delete item.data_destruction
-                    }
+                if (api_name) {
+                    delete item.data_destruction
+                }
                 stocks_report.push(item)
             }
         })
